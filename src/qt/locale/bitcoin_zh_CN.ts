d061ac60b6d9949d73acd6c57ea25af4917a8158
Coin Dev <contact@infinitericks.com>
2025-11-18 17:14:30 -0300

    v2.0.0 update

--- src/qt/locale/bitcoin_zh_CN.ts
+++ src/qt/locale/bitcoin_zh_CN.ts
@@ -18,7 +18,7 @@ Copyright © 2012-2014 The NovaCoin developers
 Copyright © 2014 The InfiniteRicks developers</source>
         <translation>版权所有 © 2009-2014 比特币Bitcoin开发组
 版权所有 © 2012-2014 新星币Novacoin开发组
-版权所有 © 2014 黑币InfiniteRicks开发组</translation>
+版权所有 © 2014-2025 黑币InfiniteRicks开发组</translation>
     </message>
     <message>
         <location line="+15"/>
